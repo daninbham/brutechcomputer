3a0ae96f595c21af10ef1dca5e987c1c7fe1d0e8
Dan Brunner <daninbham@gmail.com>
2024-07-12 17:41:41 -0500

    New branch for Issue #1 Background Image

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,25 +1,30 @@
 const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("nav .container ul li");
-window.onscroll = () => {
-  var current = "";
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    if (pageYOffset >= sectionTop - 60) {
-      current = section.getAttribute("id");
-    }
-  });
-
-  navLi.forEach((li) => {
-    li.classList.remove("active");
-    if (li.classList.contains(current)) {
-      li.classList.add("active");
-    }
-  });
+window.onscroll = function () {
+  scrollFunction();
 };
 
-export function sendMail(req, res) {
+function sendMail(req, res) {
   let message =
     req.query.message || req.body.message || "BruTech Contact Request";
   res.status(200).send(message);
+}
+
+function scrollFunction() {
+  let scrollTopButton = document.getElementById("scrollTopButton");
+  if (
+    document.body.scrollTop > window.innerHeight ||
+    document.documentElement.scrollTop > window.innerHeight
+  ) {
+    scrollTopButton.style.display = "block";
+  } else {
+    scrollTopButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
 }
